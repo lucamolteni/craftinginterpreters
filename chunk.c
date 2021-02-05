7d9615fb6abee01dc02f44e2e0233eeb85513640
Luca Molteni <volothamp@gmail.com>
2021-02-05 16:36:33 +0100

    14.6 line numbers (and value printing fix)

--- chunk.c
+++ chunk.c
@@ -11,10 +11,11 @@ void initChunk(Chunk *chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte) {
+void writeChunk(Chunk *chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
@@ -22,9 +23,14 @@ void writeChunk(Chunk *chunk, uint8_t byte) {
                                  chunk->code,
                                  oldCapacity,
                                  chunk->capacity);
+        chunk->lines = GROW_ARRAY(int,
+                                  chunk->lines,
+                                  oldCapacity,
+                                  chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
 }
 
@@ -35,6 +41,7 @@ int addConstant(Chunk *chunk, Value value) {
 
 void freeChunk(Chunk *chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
