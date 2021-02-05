ce8b415059d79dde3acb22f321d250995519d040
Luca Molteni <volothamp@gmail.com>
2021-02-05 18:05:20 +0100

    15.3 binary operations

--- vm.c
+++ vm.c
@@ -36,6 +36,13 @@ static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push (a op b); \
+    } while (false)
+
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
         printf("          ");
@@ -57,10 +64,11 @@ static InterpretResult run() {
                 printf("\n");
                 break;
             }
-            case OP_NEGATE: {
-                push(-pop());
-                break;
-            }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
+            case OP_NEGATE:   push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -72,6 +80,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 
 InterpretResult interpret(Chunk *chunk) {
     vm.chunk = chunk;
